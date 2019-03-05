58bef96edf0cdf7908f54d28c1c9bb2a046bbfe7
root <sudyaroveugene@yandex.ru>
2019-03-05 08:35:32 +0700

    Именованные каналы

--- pipe2/rewriter.cpp
+++ pipe2/rewriter.cpp
@@ -13,28 +13,20 @@ main( int argc, char** argv )
 {
 	char buf[MAXBUF];
 	
-	// unlink("/home/eugene/pipe2/in.fifo"); 
-	// unlink("/home/eugene/pipe2/out.fifo"); 
-	int fifo_in = mkfifo( "/home/eugene/pipe2/in.fifo", O_RDWR );
-//	int fifo_in = mkfifo( "/home/box/in.fifo", O_RDWR | O_CREAT | O_TRUNC );
-	int fifo_out = mkfifo( "/home/eugene/pipe2/out.fifo", O_RDWR );
-//	int fifo_out = mkfifo( "/home/box/out.fifo", O_RDWR | O_CREAT | O_TRUNC );
+	int fifo_in = mkfifo( "/home/box/in.fifo", O_RDWR | O_CREAT );
+	int fifo_out = mkfifo( "/home/box/out.fifo", O_RDWR | O_CREAT );
 	if( (fifo_in==-1 || fifo_out==-1) && errno!=EEXIST )
 	{
 		printf( "Error creat fifo\n");
 		return 1;
 	}
-//	int fd_out = open( "/home/eugene/pipe2/out.fifo", O_RDWR );	
-	int fd_in = open( "/home/eugene/pipe2/in.fifo", O_RDONLY | O_NONBLOCK );	
-//	int fd_in = open( "/home/box/in.fifo", O_RDONLY );	
+	int fd_in = open( "/home/box/in.fifo", O_RDONLY | O_NONBLOCK );	
 	if( fd_in<0 )
 	{
 		printf( "Error open fifo in\n");
 		return 1;
 	}	
-	
-	int fd_out = open( "/home/eugene/pipe2/out.fifo", O_RDWR, 0666 );
-//	int fd_out = open( "/home/box/out.fifo", O_WRONLY );	
+	int fd_out = open( "/home/box/out.fifo", O_RDWR, 0666 );	
 	if( fd_out<0 )
 	{
 		printf( "Error open fifo out\n");
